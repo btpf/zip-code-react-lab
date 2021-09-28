5ed7d04ff2fc18885f5d5bb214241b7857648128
btpf <bretpapkoff@Gmail.com>
2021-09-27 22:57:16 -0400

    Complete Project 1

--- zip-search/src/App.js
+++ zip-search/src/App.js
@@ -3,25 +3,60 @@ import './App.css';
 
 
 function City(props) {
-  return (<div>This is the City component</div>);
+  return (<div id='appContainer'>
+    <div id="containerTitle">{props.locationText}</div>
+    <ul>
+      <li>State: {props.state}</li>
+      <li>Location: {props.location}</li>
+      <li>Population (estimated): {props.population}</li>
+      <li>Total Wages: {props.totalWages}</li>
+    </ul>
+  </div>);
 }
 
 function ZipSearchField(props) {
-  return (<div>This is the ZipSearchField component</div>);
+  return (<div id='zipSearch'>
+    <div>Zip Code</div>
+    <input onChange={props.changeHandler} value={props.zipCode}/>
+  </div>);
 }
 
 
 class App extends Component {
+  state = {
+    zipCode: '',
+    data:[]
+  }
+  zipChanged = (event) =>{
+    let inputLength = event.target.value.length
+    if(inputLength < 6){
+    this.setState({zipCode: event.target.value, data: []})
+    if(inputLength == 5){
+      fetch('http://ctp-zip-api.herokuapp.com/zip/' + event.target.value)
+      .then((res)=> res.json())
+      .then((data)=> this.setState({data:data}))
+    }
+  }
+}
   render() {
+    console.log(this.state.data)
     return (
       <div className="App">
         <div className="App-header">
           <h2>Zip Code Search</h2>
         </div>
-        <ZipSearchField />
+        <ZipSearchField zipCode={this.state.zipCode} changeHandler={this.zipChanged}/>
         <div>
-          <City />
-          <City />
+          {this.state.data.map((item,i)=>(
+            // <div key={i}>{item.Location}</div>
+            <City key={i} state={item.State} locationText={item.LocationText} 
+            location={'(' + item.Xaxis + ", " + item.Yaxis + ")"}
+             population={item.EstimatedPopulation}
+             totalWages={item.TotalWages}
+             />
+            ))}
+          {/* <City />
+          <City /> */}
         </div>
       </div>
     );
